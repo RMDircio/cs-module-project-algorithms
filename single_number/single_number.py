729aea5ac68c6085d5c79742f9529492d272054a
RMDircio <reginadircio@gmail.com>
2020-10-12 22:32:01 -0500

    upated

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -5,7 +5,16 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # empty list to store unique element
+    unique_num = []
+    # loop through list for unique element
+    for i in arr:
+        # if element is not in list
+        if i not in unique_num:
+            unique_num.append(i)
+    for i in unique_num:
+        print i
+
 
     pass
 
